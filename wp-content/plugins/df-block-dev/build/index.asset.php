ec0705cf3be07a070901e8e2620e1d8ab6dcebd4
deano252 <dforant@gmail.com>
2023-03-28 08:58:18 -0400

    cleanup

--- wp-content/plugins/df-block-dev/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '2fb258aa354b7593a452');
--- wp-content/plugins/df-block-dev/src/save.js
+++
@@ -1,9 +0,0 @@
-import {useBlockProps} from '@wordpress/block-editor'
-
-const Save = () =>{
-    const blockProps = useBlockProps.save()
-
-    return <p {...blockProps}>Hello World! From the Frontend. From our save.js file</p>
-}
-
-export default Save
