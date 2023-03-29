589f45c64a75ec993805463e237f3137e2257449
deano252 <dforant@gmail.com>
2023-03-29 17:19:41 -0400

    block code cleanup

--- wp-content/plugins/df-my-first-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '783db0eb4b6b951f0def');
--- wp-content/plugins/df-my-first-block/src/save.js
+++
@@ -1,7 +0,0 @@
-import { useBlockProps } from "@wordpress/block-editor";
-
-const Save = () => {
-    const blockProps = useBlockProps.save()
-    return <p {...blockProps}>Hello world, from the frontend. From the save.js file.</p>
-}
-export default Save
