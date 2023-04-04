fd2a04f3953d0ea5324a7896b572fc7b96d4d447
deano252 <dforant@gmail.com>
2023-04-04 05:53:41 -0400

    unstage plugin code

--- block-code/01-Basic-block-intro/src/edit.js
+++
@@ -1,7 +0,0 @@
-import {useBlockProps} from '@wordpress/block-editor'
-
-const edit = () => {
-    return <p {...useBlockProps()}>Hello world, from editor.</p>
-}
-
-export default edit
--- wp-content/plugins/df-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'fb590e20291b20d271b3');
